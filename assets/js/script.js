f02b446f71684b49cc256574ad042f4979adced5
emilykidd3 <e.kidd61@yahoo.com>
2020-12-17 22:25:55 -0800

    console log shows local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -273,6 +273,45 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+// load tasks from localStorage // looks like createTaskEl()
+var loadTasks = function () {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (tasks === null) {
+        tasks = [];
+        return false;
+    }
+
+    for (var i = 0; i < tasks.length; i++) {
+        taskIdCounter = tasks[i];
+        console.log(tasks[i]);
+        var listItemEl = document.createElement("li");
+        listItemEl.className="task-item";
+        listItemEl.setAttribute("data-task-id", tasks[i].id);
+        listItemEl.setAttribute("draggable", true);
+        var taskInfoEl = document.createElement("div");
+        taskInfoEl.className="task-info";
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+
+        listItemEl.appendChild(taskInfoEl);
+
+        var taskActionsEl = createTaskActions(tasks[i].id);
+        listItemEl.appendChild(taskActionsEl);
+
+        if (tasks[i].status === "to do") {
+            listItemEl.querySelector("select[name='status-change']".selectedIndex) = 0;
+            listItemEl.appendChild(tasksToDoEl);
+        }
+        else if (tasks[i].status === "complete") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            listItemEl.appendChild(tasksCompletedEl);
+        }
+        taskIdCounter++;
+
+        console.log(listItemEl);
+    }
+
+}
+
 formEl.addEventListener("submit", taskFormHandler);
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -286,3 +325,5 @@ pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
 
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
+
+loadTasks();
